6661058655ab105ee6b018c6af700c3cdbb0fd61
Tellier-Neo <blume.unit@gmail.com>
2023-10-12 09:49:11 +0200

    V3

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ var audio = new Audio("bop.mp3");
 bop.addEventListener("mouseenter", function() {
   audio.play();
 });
+
+
+
+function changerStyle() {
+  var stylesheet = document.getElementById('stylesheet');
+  if (stylesheet.getAttribute('href') === 'main_dark.css') {
+      stylesheet.setAttribute('href', 'main_light.css'); // Remplacez 'style2.css' par le nom de votre deuxième feuille de style.
+  } else {
+      stylesheet.setAttribute('href', 'main_dark.css'); // Remplacez 'style1.css' par le nom de votre première feuille de style.
+  }
+}
